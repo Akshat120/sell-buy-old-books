0fac3a1d6db1d9ff0dec68caee33201dac589bac
Zepurts <akshat.dhiman15@gmail.com>
2021-10-28 12:20:09 +0530

    views added.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,6 +3,27 @@ exports.home = function(req,res){
     res.render(`home/home.ejs`);
 }
 
+exports.books = function(req,res){
+    res.render(`home/books.ejs`);   
+}
+
+exports.buy = function(req,res){
+    res.render(`home/buy.ejs`);   
+}
+
+exports.sell = function(req,res){
+    res.render(`home/sell.ejs`);   
+}
+
+exports.login = function(req,res){
+    res.render(`home/login.ejs`);   
+}
+
+exports.register = function(req,res){
+    res.render(`home/register.ejs`);   
+}
+
+
 
 
 
--- router.js
+++ router.js
@@ -5,5 +5,10 @@ const homeController = require('./controllers/homeController');
 
 // home-route
 router.get('/',homeController.home);
+router.get('/books',homeController.books);
+router.get('/buy',homeController.buy);
+router.get('/sell',homeController.sell);
+router.get('/login',homeController.login);
+router.get('/register',homeController.register);
 
 module.exports = router;
